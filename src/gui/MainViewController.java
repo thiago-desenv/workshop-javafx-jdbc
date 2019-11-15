9ea0c10274a952abc34026c975f8a7ac677cb7cb
Thiago Xavier <thiago_osim@hotmail.com>
2019-11-15 14:38:43 -0300

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -30,7 +30,7 @@ public class MainViewController implements Initializable  {
 	}
 	
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	public void onMenuItemAboutAction() {
